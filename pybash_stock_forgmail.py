2abcd9809c1be8b0e2f9042a5a729cecc7338849
hautinboy <hautinboy@yahoo.com.tw>
2015-04-12 12:44:56 +0800

    將輸出文字對齊

--- pybash_stock_forgmail.py
+++ pybash_stock_forgmail.py
@@ -14,7 +14,6 @@ from grs import TWSENo
 from grs import OTCNo
 
 
-
 Stock_no_name = TWSENo().all_stock  # 所有上市股票名稱與代碼字典 type: dict
 
 OTC_no_name = OTCNo().all_stock     # 所有上櫃股票名稱與代碼字典 type: dict
@@ -28,10 +27,10 @@ OTC_no_list = OTCNo().all_stock_no # 所有上櫃股票代碼
 #OTC_name_list = OTCNo().all_stock_name
 
 
-content = "贏要衝,輸要縮.  生死有命,富貴在天."   #沒有辦法換行
+content = "贏要衝,輸要縮."   #沒有辦法換行
 
 time_now = datetime.now().strftime("%Y-%m%d") #今天的日期 ex:2015-0411
-title = str(time_now+"-小小兵機器人") #Email郵件的標題 ex:2015-0411-選股機器人
+title = str(time_now+"-小小兵盤後機器人") #Email郵件的標題 ex:2015-0411-選股機器人
 
 attachment = str(time_now)+'.txt' #附件名稱使用當日時間 ex:2015-0411.txt
 
@@ -42,45 +41,59 @@ ID = f.readline().strip('\n') #不包含換行符號\n
 PW = f.readline().strip('\n')
 
 
-
 fileopen.write('上市公司股票篩選\n\n\n')
-
 
 j = 1
 for i in stock_no_list:
-    print i, '上市', Stock_no_name[i]
+    #print i, '上市', Stock_no_name[i]
     try:
         best_point, info = BestFourPoint(Stock(i)).best_four_point()
        
         if best_point:           # 買點
+           ### fileopen.write(str(j)+' '+'Buy'+' '+i+' ('+Stock_no_name[i].encode("UTF-8")+')'\
+           ### +'  成交張數(含零股)  '+ str(Stock(i).raw[-1][1]/1000) +                 '\n')
+
+           ## txt = "%-2d Buy%6s%10s 成交張數(包含零股):%9.1f \n"%(j,i,Stock_no_name[i].encode("UTF-8"),Stock(i).raw[-1][1]/1000)
+           ## fileopen.write(txt)
+
+
             fileopen.write(str(j)+' '+'Buy'+' '+i+' ('+Stock_no_name[i].encode("UTF-8")+')'+'\n')
-            fileopen.write(info.encode("UTF-8"))
+            fileopen.write('  成交張數:'+str(int(Stock(i).raw[-1][1]/1000))+'\n')
+
+
+
+            fileopen.write('  '+info.encode("UTF-8"))
             fileopen.write('\n\n')
             fileopen.write('----------------------------------\n')
            
             j+=1
     except:     # 不作為或資料不足
         pass
-    
 fileopen.write('\n\n\n上櫃公司股票篩選\n\n\n')
+
 
 j = 1
 for i in OTC_no_list:
-    print i,'上櫃', OTC_no_name[i]
+    #print i,'上櫃', OTC_no_name[i]
     try:
         best_point, info = BestFourPoint(Stock(i)).best_four_point()
 
         if best_point:           # 買點
-            fileopen.write(str(j)+' '+'Buy'+' '+i+' ('+OTC_no_name[i].encode("UTF-8")+')'+'\n')
+            #fileopen.write(str(j)+' '+'Buy'+' '+i+' ('+OTC_no_name[i].encode("UTF-8")+')'\
+            #+'  成交張數  '+ str(Stock(i).raw[-1][1]) +                 '\n')
+
+
+            txt = "%-2d Buy%6s%10s 成交張數:%-9d \n"%(j,i,OTC_no_name[i].encode("UTF-8"),Stock(i).raw[-1][1])
+            fileopen.write(txt)
+
+
             fileopen.write(info.encode("UTF-8"))
             fileopen.write('\n\n')
             fileopen.write('----------------------------------\n')
             j+=1
     except:     # 不作為或資料不足
         pass
-
-    
-
+   
 fileopen.close()                #關閉檔案
 
 
@@ -107,7 +120,8 @@ os.system('sendEmail -o \
  -m %s \
  -a %s \
  '%(ID, PW, title, content, attachment))
- 
+
+
  
  
  
