a088b4f4f611ae45fc093a1416c83a22d2bd7163
hautinboy <hautinboy@yahoo.com.tw>
2015-04-17 00:49:37 +0800

    加入上市上櫃M日黃金交叉N日,並有回測天數;加入執行時間. 12

--- pybash_stock_forgmail.py
+++ pybash_stock_forgmail.py
@@ -12,6 +12,11 @@ from grs import BestFourPoint
 from grs import Stock
 from grs import TWSENo
 from grs import OTCNo
+import time, itertools
+
+
+# 記錄開始時間 Record start time
+tStart = time.time()
 
 
 Stock_no_name = TWSENo().all_stock  # 所有上市股票名稱與代碼字典 type: dict
@@ -20,11 +25,7 @@ OTC_no_name = OTCNo().all_stock     # 所有上櫃股票名稱與代碼字典 ty
 
 stock_no_list = TWSENo().all_stock_no # 所有上市股票代碼
 
-#stock_name_list = TWSENo().all_stock_name
-
 OTC_no_list = OTCNo().all_stock_no # 所有上櫃股票代碼
-
-#OTC_name_list = OTCNo().all_stock_name
 
 
 content = "贏要衝,輸要縮."   #沒有辦法換行
@@ -40,122 +41,125 @@ f = open('/home/tim/GMAIL.txt','r') #於前一個相對目錄中放置登入GMAI
 ID = f.readline().strip('\n') #不包含換行符號\n
 PW = f.readline().strip('\n')
 
+"""
 
 fileopen.write('上市公司股票篩選\n\n\n')
 
-"""
-j = 1
-for i in stock_no_list:
-    #print i, '上市', Stock_no_name[i]
-    try:
-        best_point, info = BestFourPoint(Stock(i)).best_four_point()
-        if best_point:           # 買點
-           ## fileopen.write(str(j)+' '+'Buy'+' '+i+' ('+Stock_no_name[i].encode("UTF-8")+')'\
-           ### +'  成交張數(含零股)  '+ str(Stock(i).raw[-1][1]/1000) +                 '\n')
 
-           ## txt = "%-2d Buy%6s%10s 成交張數(包含零股):%9.1f \n"%(j,i,Stock_no_name[i].encode("UTF-8"),Stock(i).raw[-1][1]/1000)
-           ## fileopen.write(txt)
-            fileopen.write(str(j)+' '+'Buy'+' '+i+' ('+Stock_no_name[i].encode("UTF-8")+')'+'\n')
-            fileopen.write('  成交張數:'+str(int(Stock(i).raw[-1][1]/1000))+'\n')
-            fileopen.write('  '+info.encode("UTF-8"))
-            fileopen.write('\n\n')
-            fileopen.write('----------------------------------\n')
-            j+=1
-    except:     # 不作為或資料不足
-        pass
-"""
-        
-"""
-j = 1
-for i in stock_no_list:
-    try:
-        best_point, info = BestFourPoint(Stock(i)).best_four_point()
-        # 要執行這段程式 best_buy_or_sell.py下的best_four_point 要改成 return None ,None
-    except ValueError: #印出三個月內有資料不足的股票,可能當日都沒交易傳回 - 字串
-        print 'ValueError',i 
-        # 顯示ValueError表示該各股兩個月內有其中一天沒交易量,所以抓到字串" - "
-    else:
-	pass
-"""
-
-fileopen.write("\n"+"新豪式黃金交叉定義:今天短均線高於長均線,且前回測N天中其短均線皆要低於長均線"+"\n\n")
+fileopen.write("\n"+"新豪式黃金交叉定義:今天短均線高於長均線,且回測N天內的短均線皆要低於長均線"+"\n\n")
 
 #新豪式黃金交叉定義:今天短均線高於長均線,且前回測N天中其短均線皆要低於長均線
+
+#=====================
+index = 1 
+for i in stock_no_list:
+    try:
+        if BestFourPoint(Stock(i)).golden_cross(m=5,n=10,back_to_test_n_days=5) and (BestFourPoint(Stock(i)).data.moving_average(5)[0][-1] >  BestFourPoint(Stock(i)).data.moving_average(10)[0][-1] ):
+           print i,'123'         # 5日均線黃金交叉10日均線,並且要回測5天.
+           fileopen.write(str(index)+" "+"新豪式黃金交叉(5日均線向上穿越10日均線且符合5天回測)"+"-"+Stock_no_name[i].encode("UTF-8")+"-"+i+"-"+"成交張數"+"-"+str(int(Stock(i).raw[-1][1]/1000))+"\n")
+           index = index + 1 
+    except:     # 回傳為None 或 資料不足導致ValueError
+        pass
+
+#=====================
+fileopen.write("\n\n")
+index = 1
+for i in stock_no_list:
+    try:
+        if BestFourPoint(Stock(i)).golden_cross(m=5,n=10,back_to_test_n_days=10) and (BestFourPoint(Stock(i)).data.moving_average(5)[0][-1] >  BestFourPoint(Stock(i)).data.moving_average(10)[0][-1] ):
+           print i,'123'         # 5日均線黃金交叉10日均線,並且要回測10天.
+           fileopen.write(str(index)+" "+"新豪式黃金交叉(5日均線向上穿越10日均線且符合10天回測)"+"-"+Stock_no_name[i].encode("UTF-8")+"-"+i+"-"+"成交張數"+"-"+str(int(Stock(i).raw[-1][1]/1000))+"\n")
+           index = index + 1
+    except:     # 回傳為None 或 資料不足導致ValueError
+        pass
+
+#=====================
+fileopen.write("\n\n")
+index = 1
+for i in stock_no_list:
+    try:
+        if BestFourPoint(Stock(i)).golden_cross(m=5,n=20,back_to_test_n_days=5) and (BestFourPoint(Stock(i)).data.moving_average(5)[0][-1] >  BestFourPoint(Stock(i)).data.moving_average(20)[0][-1] ):
+           print i,'123'         # 5日均線黃金交叉20日均線,並且要回測5天.
+           fileopen.write(str(index)+" "+"新豪式黃金交叉(5日均線向上穿越20日均線且符合5天回測)"+"-"+Stock_no_name[i].encode("UTF-8")+"-"+i+"-"+"成交張數"+"-"+str(int(Stock(i).raw[-1][1]/1000))+"\n")
+           index = index + 1     
+    except:     # 回傳為None 或 資料不足導致ValueError
+        pass
+
+#=====================
+fileopen.write("\n\n")
 index = 1
 for i in stock_no_list:
     try:
         if BestFourPoint(Stock(i)).golden_cross(m=5,n=20,back_to_test_n_days=10) and (BestFourPoint(Stock(i)).data.moving_average(5)[0][-1] >  BestFourPoint(Stock(i)).data.moving_average(20)[0][-1] ):
            print i,'123'         # 5日均線黃金交叉20日均線,並且要回測10天.
-           fileopen.write(str(index)+" "+"新豪式黃金交叉(5日均線向上穿越20日均線回測10天)"+"-"+Stock_no_name[i].encode("UTF-8")+"-"+i+"-"+"成交張數"+"-"+str(int(Stock(i).raw[-1][1]/1000))+"\n")
+           fileopen.write(str(index)+" "+"新豪式黃金交叉(5日均線向上穿越20日均線且符合10天回測)"+"-"+Stock_no_name[i].encode("UTF-8")+"-"+i+"-"+"成交張數"+"-"+str(int(Stock(i).raw[-1][1]/1000))+"\n")
            index = index + 1
     except:     # 回傳為None 或 資料不足導致ValueError
         pass
 
-
-#BestFourPoint(Stock(i)).golden_cross(m=5,n=20,back_to_test_n_days=10)
-#BestFourPoint(Stock(i)).data.moving_average(5)[0][-1] >  BestFourPoint(Stock(i)).data.moving_average(20)[0][-1] 
-"""
-fileopen.write("\n"+"傳統黃金交叉定義:昨天短均線低於長均線,今天短均線高於長均線"+"\n\n")
-
-#傳統黃金交叉(5日均線向上穿越20日均線)
+#=====================
+fileopen.write("\n\n")
 index = 1
 for i in stock_no_list:
-    try: 
-        if BestFourPoint(Stock(i)).golden_cross_no_back_test(m=5,n=20):
-           fileopen.write(str(index)+" "+"傳統黃金交叉(5日均線向上穿越20日均線)"+"-"+Stock_no_name[i].encode("UTF-8")+"-"+i+"-"+"成交張數"+"-"+str(int(Stock(i).raw[-1][1]/1000))+"\n")
+    try:
+        if BestFourPoint(Stock(i)).golden_cross(m=5,n=20,back_to_test_n_days=15) and (BestFourPoint(Stock(i)).data.moving_average(5)[0][-1] >  BestFourPoint(Stock(i)).data.moving_average(20)[0][-1] ):
+           print i,'123'         # 5日均線黃金交叉20日均線,並且要回測15天.
+           fileopen.write(str(index)+" "+"新豪式黃金交叉(5日均線向上穿越20日均線且符合15天回測)"+"-"+Stock_no_name[i].encode("UTF-8")+"-"+i+"-"+"成交張數"+"-"+str(int(Stock(i).raw[-1][1]/1000))+"\n")
            index = index + 1
     except:     # 回傳為None 或 資料不足導致ValueError
         pass
-"""
 
-
-
-
-"""
-j =1
+#=====================
+fileopen.write("\n\n")
+index = 1
 for i in stock_no_list:
-        try:
-                golden_cross_true_or_false = BestFourPoint(Stock(i)).golden_cross() #判斷黃金交叉
-                # 今天以前的五天內五日均線小於20日均線,但是今天5日均線高於20日均線,故今天為黃金交叉
-                if golden_cross_true_or_false:
-                        print i
-                        fileopen.write(str(j)+' '+'黃金交叉-Buy'+' '+i+' ('+Stock_no_name[i].encode("UTF-8")+')'+'\n')
-        except:
-                pass
+    try:
+        if BestFourPoint(Stock(i)).golden_cross(m=5,n=20,back_to_test_n_days=20) and (BestFourPoint(Stock(i)).data.moving_average(5)[0][-1] >  BestFourPoint(Stock(i)).data.moving_average(20)[0][-1] ):
+           print i,'123'         # 5日均線黃金交叉20日均線,並且要回測20天.
+           fileopen.write(str(index)+" "+"新豪式黃金交叉(5日均線向上穿越20日均線且符合20天回測)"+"-"+Stock_no_name[i].encode("UTF-8")+"-"+i+"-"+"成交張數"+"-"+str(int(Stock(i).raw[-1][1]/1000))+"\n")
+           index = index + 1
+    except:     # 回傳為None 或 資料不足導致ValueError
+        pass
+
 """
-
-
 
  
 fileopen.write('\n\n\n上櫃公司股票篩選\n\n\n')
 
-"""
-j = 1
+fileopen.write("\n"+"新豪式黃金交叉定義:今天短均線高於長均線,且回測N天內的短均線皆要低於長均線"+"\n\n")
+
+#新豪式黃金交叉定義:今天短均線高於長均線,且前回測N天中其短均線皆要低於長均線
+
+#=====================
+index = 1 
 for i in OTC_no_list:
-    #print i,'上櫃', OTC_no_name[i]
     try:
-        best_point, info = BestFourPoint(Stock(i)).best_four_point()
-
-        if best_point:           # 買點
-            #fileopen.write(str(j)+' '+'Buy'+' '+i+' ('+OTC_no_name[i].encode("UTF-8")+')'\
-            #+'  成交張數  '+ str(Stock(i).raw[-1][1]) +                 '\n')
-
-
-            txt = "%-2d Buy%6s%10s 成交張數:%-9d \n"%(j,i,OTC_no_name[i].encode("UTF-8"),Stock(i).raw[-1][1])
-            fileopen.write(txt)
-
-
-            fileopen.write(info.encode("UTF-8"))
-            fileopen.write('\n\n')
-            fileopen.write('----------------------------------\n')
-            j+=1
-    except:     # 不作為或資料不足
+        if BestFourPoint(Stock(i)).golden_cross(m=5,n=10,back_to_test_n_days=5) and (BestFourPoint(Stock(i)).data.moving_average(5)[0][-1] >  BestFourPoint(Stock(i)).data.moving_average(10)[0][-1] ):
+           print i,'123'         # 5日均線黃金交叉10日均線,並且要回測5天.
+           fileopen.write(str(index)+" "+"新豪式黃金交叉(5日均線向上穿越10日均線且符合5天回測)"+"-"+OTC_no_name[i].encode("UTF-8")+"-"+i+"-"+"成交張數"+"-"+str(int(Stock(i).raw[-1][1]))+"\n")
+           index = index + 1 
+    except:     # 回傳為None 或 資料不足導致ValueError
         pass
-"""   
+
+#=====================
+#fileopen.write("\n\n")
+#index = 1
+#for i in OTC_no_list:
+#    try:
+#        if BestFourPoint(Stock(i)).golden_cross(m=5,n=10,back_to_test_n_days=10) and (BestFourPoint(Stock(i)).data.moving_average(5)[0][-1] >  BestFourPoint(Stock(i)).data.moving_average(10)[0][-1] ):
+#           print i,'123'         # 5日均線黃金交叉10日均線,並且要回測5天.
+#           fileopen.write(str(index)+" "+"新豪式黃金交叉(5日均線向上穿越10日均線且符合10天回測)"+"-"+OTC_no_name[i].encode("UTF-8")+"-"+i+"-"+"成交張數"+"-"+str(int(Stock(i).raw[-1][1]))+"\n")
+#           index = index + 1
+#    except:     # 回傳為None 或 資料不足導致ValueError
+#        pass
+
+
+
+
+
 fileopen.close()                #關閉檔案
 
 
-"""
 os.system('sendEmail -o \
  -f u160895@taipower.com.tw \
  -t "WEI <weihautin@gmail.com>" u160895@taipower.com.tw \
@@ -166,9 +170,12 @@ os.system('sendEmail -o \
  -m %s \
  -a %s \
  '%(ID, PW, title, content, attachment))
-"""
 
  
+# 記錄結束時間 Record stop time
+tStop = time.time()
+
+print(tStop - tStart)
  
  
 
