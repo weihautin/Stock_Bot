059bf0a455cc7058b9b2297c99c60407d0fa57c4
“TPC” <weihautin@gmail.com>
2015-04-23 11:24:06 +0800

    加入抓取是否開放現股當沖sell_buy_immediately.py

--- csvv_ex.py
+++ csvv_ex.py
@@ -6,9 +6,11 @@ Created on Wed Apr 22 11:11:23 2015
 """
 
 from csvv import yields as a
+from sell_buy_immediately import stock_buy_sell_oneday as b
 
 
 
 if __name__ == '__main__':
     pass
     print a()['1101'][0]
+    print b()['1101'][0]
