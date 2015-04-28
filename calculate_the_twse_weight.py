ed0834ca2c147db69897d9aae8480d4a470c2a11
“TPC” <weihautin@gmail.com>
2015-04-28 19:35:43 +0800

    加入統計各股貢獻打盤多少點數

--- calculate_the_twse_weight.py
+++ calculate_the_twse_weight.py
@@ -23,7 +23,7 @@ def twse_weight_percent():
     回傳大盤加權指數佔比
     """
     stock_weight_percent = []
-    f = open('stock_weighted1.csv','r')
+    f = open('stock_weighted2.csv','r')
     for row in csv.reader(f):
         try:
             #stock_weight_percent.append([row[1],row[2].decode('UTF-8'), row[3]])
@@ -36,15 +36,19 @@ def twse_weight_percent():
 if __name__ == "__main__":
     pass
 
-    fileopen = open("cal_twse_weight.txt", 'w')
+    fileopen = open("cal_twse_weight.csv", 'w')
     
     a = twse_weight_percent()
-    print a
+    #print a
+    
+    
+    
+    
     #print a['1101'][1], '殖益率'
     b = Twse_Weighted_Index() #大盤加權指數
-    print int(b.Weighted_Index_average(1,b.serial_fetch(month=1))[-1])
+    #print int(b.Weighted_Index_average(1,b.serial_fetch(month=1))[-1])
 
-    today_weight = b.Weighted_Index_average(1,b.serial_fetch(month=1))[-1]    
+    today_weight = b.Weighted_Index_average(1,b.serial_fetch(month=1))[-1]    # -1今天收盤 -2昨天收盤
     
     # Stock('1101').raw[-1][-2] 漲跌點數
     # Stock('1101').raw[-1][4] 昨天收盤價
@@ -56,15 +60,16 @@ if __name__ == "__main__":
     
     total = 0    
     for i in a:
+       
         contribution = 0
-        contribution = float(Stock(i[0]).raw[-1][-2]) / float(Stock(i[0]).raw[-1][4]) * today_weight * float(i[2]) / 100
+        contribution = float(Stock(i[0]).raw[-1][-2]) / float(Stock(i[0]).raw[-2][-3]) * today_weight * float(i[2]) / 100
         total = total + contribution
-        print i[0], contribution, total
         
-    
-    
-    fileopen.write('\n\n\n上櫃公司股票篩選\n\n\n')
-    
+        print i[0],i[1],contribution,total      
+        
+        
+        fileopen.write(str(i[0])+','+str(i[1])+','+str(contribution)+','+ str(total)+"\n")
+        
     fileopen.close()    #關閉檔案
     
     
