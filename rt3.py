38ae463201da8e063b84d60ad70b3329133d258d
“TPC” <weihautin@gmail.com>
2015-04-29 11:42:04 +0800

    完成即時分析成交量過週均線個股rt3.py

--- rt3.py
+++ rt3.py
@@ -20,12 +20,11 @@ stock_no_list = TWSENo().all_stock_no # 所有上市股票代碼
 
 content = "贏要衝,輸要縮."   #沒有辦法換行
 time_now = datetime.now().strftime("%Y-%m%d-%H%M") #今天的日期 ex:2015-0411
-time_hour = datetime.now().strftime("%H%M")
 title = str(time_now+"-盤中即時機器人") #Email郵件的標題 ex:2015-0411-選股機器人
 
-attachment = str(time_now)+'-realtimefilter'+'.csv' #附件名稱使用當日時間 ex:2015-0411.txt
+attachment = str(time_now)+'.csv' #附件名稱使用當日時間 ex:2015-0411.txt
 
-fileopen = open(attachment,'w')
+fileopen = open(attachment, 'w') #開啟檔案,w沒有該檔案就新增
 
 f = open('/home/tim/GMAIL.txt','r') #於前一個相對目錄中放置登入GMAIL帳號密碼,目的為了不再GitHub顯示出來.
 ID = f.readline().strip('\n') #不包含換行符號\n:q
@@ -36,17 +35,16 @@ PW = f.readline().strip('\n')
 fileopen.write('上市公司股票篩選\n\n\n')
 
 
-fileopen.write("\n"+"目前該股累積成交量>昨天成交量"+"\n\n")
 
 #=====================
 index = 1
 for i in stock_no_list:
-    print i
+    #print i
     realtime_data = RealtimeTWSE(i)
     try:
-        if realtime_data.data[i]['volume_acc'] > Stock(i,mons=1).moving_average(5)[0][-1]: #今天的量大於5日週均量
+        if realtime_data.data[i]['volume_acc'] > Stock(i,mons=1).moving_average_value(5)[0][-2] or realtime_data.data[i]['volume_acc'] > Stock(i,mons=2).moving_average_value(20)[0][-2]: #今天的量大於5日週均量
         #if realtime_data.data[i]['volume_acc'] > Stock(i,mons=1).raw[-2][1]/1000: #今天的量大於昨天的量 
-           print i,'RT_OK'         #暴量長紅2天
+           print i,'Yes123'         #暴量長紅2天
            try:
                if oneday()[i][1] == '':
                   one_day = "買賣現沖 "
@@ -57,11 +55,13 @@ for i in stock_no_list:
            except:
                one_day = "" #csv找不到該股票代碼,即不開放買賣現沖
 
-           fileopen.write(str(index)+" "+time_now+"-----目前該股累積成交量>週均量"+"-"+Stock_no_name[i].encode("UTF-8")+"-"+i+"-"+"成交張數"+"-"+str(int(Stock(i,mons=1).raw[-1][1]/1000))+"-"+"殖益率"+str(fields()[i][2])+"-"+one_day+"\n")
+#           fileopen.write(str(index)+" "+time_now+"-----目前該股累積成交量>週均量"+"-"+Stock_no_name[i].encode("UTF-8")+"-"+i+"-"+"成交張數"+"-"+str(int(Stock(i,mons=1).raw[-1][1]/1000))+"-"+"殖益率"+str(fields()[i][2])+"-"+one_day+"\n")
 
-           #fileopen.write(','+str(i[1])+','+str(contribution)+','+ str(total)+"\n")
-           #fileopen.write("時間"+time_hour+Stock_no_name[i].encode("UTF-8")+"-"+i+"\n")
-           #fileopen.write(Stock_no_name[i].encode("UTF-8")+"\n")
+           fileopen.write(i+"-"+Stock_no_name[i].encode("UTF-8")+"-"+"目前累積成交量"+","+        \
+           str(float(realtime_data.data[i]['volume_acc'])/float(Stock(i,mons=1).moving_average_value(5)[0][-2]))+","+"倍週均量"+  \
+        ","+ "成交張數"+"-"+str(int(Stock(i,mons=1).raw[-1][1]/1000))+","+"殖益率"+str(fields()[i][2])+"-"+one_day+ \
+        ","+str(float(realtime_data.data[i]['volume_acc'])/float(Stock(i,mons=2).moving_average_value(20)[0][-2]))+","+"倍月均量"+ \
+                                                                                            "\n")
 
            index = index + 1
     except:     # 回傳為None 或 資料不足導致ValueError
@@ -69,10 +69,6 @@ for i in stock_no_list:
 
 
 fileopen.close()                #關閉檔案
-
-
-
-
 
 #realtime_data = RealtimeTWSE(1101)
 
