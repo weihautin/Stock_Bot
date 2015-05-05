b3037de523255e61448e3cd7bd0b5ede0bd5ddea
“TPC” <weihautin@gmail.com>
2015-05-05 14:59:38 +0800

    加入上櫃成交量即時抓取

--- fetch_realtime-next.py
+++ fetch_realtime-next.py
@@ -1,22 +1,28 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Apr 24 13:22:59 2015
+上市上櫃即時成交量計算
 
-@author: tim
 """
 
 import os
 from grs import RealtimeTWSE
+from grs import RealtimeOTC
 from datetime import datetime
 from grs import Stock
 from grs import TWSENo
-from csvv import yields as fields #殖益率
+from grs import OTCNo
+from csvv import yields as fields #TWSE殖益率
+from csvv import yields_otc as fields_otc #OTC殖益率
 from sell_buy_immediately import stock_buy_sell_oneday as oneday #是否為現股當充
 
 
 Stock_no_name = TWSENo().all_stock  # 所有上市股票名稱與代碼字典 type: dict
 
 stock_no_list = TWSENo().all_stock_no # 所有上市股票代碼
+
+OTC_no_name = OTCNo().all_stock     # 所有上櫃股票名稱與代碼字典 type: dict
+
+OTC_no_list = OTCNo().all_stock_no # 所有上櫃股票代碼
 
 
 content = "贏要衝,輸要縮."   #沒有辦法換行
@@ -32,7 +38,7 @@ ID = f.readline().strip('\n') #不包含換行符號\n:q
 
 PW = f.readline().strip('\n')
 
-
+"""
 fileopen.write('上市公司股票篩選\n\n\n')
 
 #fileopen.write("股票",",","100",",","倍週均量",",","成交張數",",","殖利率","100",",","倍月均量")
@@ -46,7 +52,7 @@ for i in stock_no_list:
     try:
         if realtime_data.data[i]['volume_acc'] > Stock(i,mons=3).moving_average_value(5)[0][-2] or realtime_data.data[i]['volume_acc'] > Stock(i,mons=3).moving_average_value(20)[0][-2]: #今天的量大於5日週均量
         #if realtime_data.data[i]['volume_acc'] > Stock(i,mons=1).raw[-2][1]/1000: #今天的量大於昨天的量 
-           print i,'Yes123'         #暴量長紅2天
+           print i,'TWSE123'        #暴量長紅2天
            try:
                if oneday()[i][1] == '':
                   one_day = "買賣現沖 "
@@ -69,6 +75,33 @@ for i in stock_no_list:
            index = index + 1
     except:     # 回傳為None 或 資料不足導致ValueError
         pass
+
+"""
+
+
+fileopen.write('上櫃公司股票篩選\n\n\n')
+
+#=====================
+index = 1
+for i in OTC_no_list:
+    #print i
+    realtime_data = RealtimeOTC(i)
+    try:
+        if realtime_data.data[i]['volume_acc'] > Stock(i,mons=3).moving_average_value(5)[0][-2]*1000 or realtime_data.data[i]['volume_acc'] > Stock(i,mons=3).moving_average_value(20)[0][-2]*1000: #今天的量大於5日週均量
+        #if realtime_data.data[i]['volume_acc'] > Stock(i,mons=1).raw[-2][1]/1000: #今天的量大於昨天的量 
+           print i,'OTC123'         #暴量長紅2天
+
+           fileopen.write(i+"-"+OTC_no_name[i].encode("UTF-8")+"-"+"目前累積成交量"+","+ \
+           str(float(realtime_data.data[i]['volume_acc'])/float(Stock(i,mons=3).moving_average_value(5)[0][-2]*1000))+","+"倍週均量"+  \
+        ","+ "成交張數"+"-"+str(realtime_data.data[i]['volume_acc'])+","+"殖益率"+str(fields_otc()[i][2])+"-"+ \
+        ","+str(float(realtime_data.data[i]['volume_acc'])/float(Stock(i,mons=3).moving_average_value(20)[0][-2]*1000))+","+"倍月均量"+ \
+                                                                                            "\n")
+# str(int(Stock(i,mons=1).raw[-1][1]/1000))
+
+           index = index + 1
+    except:     # 回傳為None 或 資料不足導致ValueError
+        pass
+
 
 
 fileopen.close()                #關閉檔案
